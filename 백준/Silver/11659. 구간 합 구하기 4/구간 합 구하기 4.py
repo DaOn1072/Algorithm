397e79066ceb4cc5923f414b7d36a2efe2ee3742
이다온 <139777054+DaOn1072@users.noreply.github.com>
2025-02-10 14:26:41 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 236 ms, Memory: 41144 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -0,0 +1,14 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+myList = list(map(int, sys.stdin.readline().split()))
+sumList = [0]
+sum = 0
+
+for i in myList:
+    sum += i
+    sumList.append(sum)
+    
+for i in range(m):
+    s, e = map(int, sys.stdin.readline().split())
+    print(sumList[e]-sumList[s-1])
