d839308c28c5208236bc2c8935ee59879d9d187b
이다온 <139777054+DaOn1072@users.noreply.github.com>
2025-02-10 22:15:02 +0900

    [Gold IV] Title: 좋다, Time: 1328 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/1253. 좋다/좋다.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+A = list(map(int, input().split()))
+A.sort()
+cnt = 0
+
+for k in range(n):
+    find = A[k]
+    i = 0
+    j = n-1
+    
+    while i < j:
+        if A[i] + A[j] == find:
+            if i != k and j != k:
+                cnt += 1
+                break
+            elif i == k:
+                i += 1
+            elif j == k:
+                j -= 1
+        elif A[i] + A[j] < find:
+            i += 1
+        else:
+            j -= 1
+            
+print(cnt)
