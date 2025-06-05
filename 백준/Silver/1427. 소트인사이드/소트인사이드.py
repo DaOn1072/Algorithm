4c246581047fd9ae8ffe597c1457b9ee85c31883
이다온 <139777054+DaOn1072@users.noreply.github.com>
2025-06-05 15:34:05 +0900

    [Silver V] Title: 소트인사이드, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.py
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.py
@@ -1,16 +1,6 @@
-import sys
-print = sys.stdout.write
-A = list(input())
+n = list(map(int, str(input())))
 
-for i in range(len(A)):
-    Max = i
-    for j in range(i+1, len(A)):
-        if A[j] > A[Max]:
-            Max = j
-    if A[i] < A[Max]:
-        temp = A[i]
-        A[i] = A[Max]
-        A[Max] = temp
-        
-for i in range(len(A)):
-    print(A[i])
+n.sort(reverse=True)
+
+for i in n:
+    print(i, end="")
